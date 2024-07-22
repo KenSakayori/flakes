2e94b5757cdaf2f07f8c0a05d2c258b0e31ab66e
Ken Sakayori <kensakayori+research@gmail.com>
2024-07-22 20:33:34 +0900

    (rethfl) initial flake
    The flake is able to build ReTHFL, and the executable runs without a problem as long as hoice or z3 is used as the backend solvers.
    Eldarica is still not supported.

---
+++ rethfl/flake.nix
@@ -0,0 +1,53 @@
+{
+  # TODO: inputs should include Eldarica (and maybe PCSat)
+  inputs = {
+    opam-nix.url = "github:tweag/opam-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "opam-nix/nixpkgs";
+    hoice.url = "github:/KenSakayori/flakes/?dir=hoice";
+    ppx-cmdliner = {
+      url = "github:hammerlab/ppx_deriving_cmdliner";
+      flake = false;
+    };
+    rethfl= {
+      url = "github:hopv/rethfl";
+      flake = false;
+    };
+
+  };
+
+  outputs = { self, flake-utils, opam-nix, nixpkgs, hoice, ppx-cmdliner, rethfl }@inputs:
+    # Don't forget to put the package name instead of `throw':
+    let package = "rethfl";
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+
+        # Since pin-depends without a specific commit hash cannot be build in a
+        # pure mode, we make a repo ourselves and use pinDepens=false
+        # Note that we are exploiting the behaviour of makeOpamRepo:
+        # packages for which the version can not be inferred get dev as their version.
+        ppx-cmdliner-repo = on.makeOpamRepo ppx-cmdliner;
+        scope =
+          on.buildOpamProject
+            {
+              pinDepends =false;
+              repos = [ ppx-cmdliner-repo opam-nix.inputs.opam-repository ];
+            }
+            package rethfl { ocaml-base-compiler = "*"; };
+        overlay = final: prev:
+          {
+            rethfl = prev.rethfl.overrideAttrs (_: {
+            buildInputs = [ pkgs.makeWrapper ];
+            postFixup = ''
+                      wrapProgram $out/bin/rethfl \
+                      --set PATH ${pkgs.lib.makeBinPath [ pkgs.z3_4_8 hoice.packages.${system}.default ]}
+                      '';
+            });
+          };
+      in {
+        legacyPackages = scope.overrideScope' overlay;
+        packages.default = self.legacyPackages.${system}.${package};
+      });
+}
